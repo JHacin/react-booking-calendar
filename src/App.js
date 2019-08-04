6c5dbea15850645c905334857d385ea74d7f1f5d
JHacin <jan.hacin@gmail.com>
2019-08-04 19:25:09 +0200

    Style booking items.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,37 @@ import { rooms, bookings } from './dummyData';
 import 'react-calendar-timeline/lib/Timeline.css';
 import './App.css';
 
+function itemRenderer({ item, itemContext, getItemProps }) {
+  return (
+    <div
+      {...getItemProps({
+        style: {
+          display: "flex",
+          alignItems: "center",
+          background: item.start_time.isAfter() ? "#aaa" : "#d32f2f",
+          border: `3px solid ${ itemContext.selected ? "#fff700" : "transparent"}`,
+          borderRadius: "12.5px",
+          boxShadow: "rgba(0, 0, 0, 0.16) 0 3px 6px",
+        }
+      })}
+    >
+      <div
+        style={{
+          position: "sticky",
+          left: "0px",
+          display: "inline-block",
+          overflow: "hidden",
+          padding: "0 6px",
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap"
+        }}
+      >
+        {itemContext.title}
+      </div>
+    </div>
+  );
+}
+
 function onItemSelect(itemId, e, time) {
   console.log(`clicked item with ID ${itemId}`);
 }
@@ -22,11 +53,12 @@ function App() {
         defaultTimeStart={moment().startOf('day').add(-3, 'day')}
         defaultTimeEnd={moment().startOf('day').add(5, 'day')}
         sidebarWidth={0}
-        lineHeight={100}
+        lineHeight={52}
         itemHeightRatio={0.5}
         canMove={false}
-        onItemSelect={(itemId, e, time) => { onItemSelect(itemId, e, time) }}
-        onCanvasClick={(groupId, time, e) => { onCanvasClick(groupId, time, e) }}
+        itemRenderer={itemRenderer}
+        onItemSelect={onItemSelect}
+        onCanvasClick={onCanvasClick}
       >
         <TimelineHeaders>
           <SidebarHeader/>
