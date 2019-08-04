33dbb2f0512561c97628f995c9139b6840fd04e3
JHacin <jan.hacin@gmail.com>
2019-08-04 17:39:10 +0200

    Implement react-calendar-timeline with some custom properties & include some dummy data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import moment from 'moment';
+import Timeline, {TimelineHeaders, SidebarHeader, DateHeader } from 'react-calendar-timeline';
+import { groups, items } from './dummyData';
+import 'react-calendar-timeline/lib/Timeline.css';
 import './App.css';
+
+function onItemSelect(itemId, e, time) {
+  console.log(`clicked item with ID ${itemId}`);
+}
+
+function onCanvasClick(groupId, time, e) {
+  console.log("clicked canvas");
+}
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Timeline
+        groups={groups}
+        items={items}
+        defaultTimeStart={moment().add(-2, 'day')}
+        defaultTimeEnd={moment().add(10, 'day')}
+        sidebarWidth={0}
+        lineHeight={100}
+        itemHeightRatio={0.5}
+        canMove={false}
+        onItemSelect={(itemId, e, time) => { onItemSelect(itemId, e, time) }}
+        onCanvasClick={(groupId, time, e) => { onCanvasClick(groupId, time, e) }}
+      >
+        <TimelineHeaders>
+          <SidebarHeader/>
+          <DateHeader
+            unit="month"
+            labelFormat="MMMM"
+          />
+          <DateHeader
+            unit="day"
+            labelFormat="D"
+          />
+        </TimelineHeaders>
+      </Timeline>
     </div>
   );
 }
