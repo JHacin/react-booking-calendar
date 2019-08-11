07f4e4c9d6641937159ad210ae7880580a8805c9
JHacin <jan.hacin@gmail.com>
2019-08-11 19:10:56 +0200

    Add a fade in/out route transition effect.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AnimatedRoute } from "react-router-transition";
 import "./App.css";
 import BookingsTimeline from "./BookingsTimeline";
 import FooterMenu from "./FooterMenu";
@@ -12,13 +13,35 @@ function Settings() {
   return <div>Settings</div>
 }
 
+function AnimatedRouteHOC(props) {
+  const { path, exact, component } = props;
+
+  const atEnterOrLeave = { opacity: 0 };
+  const atActive = { opacity: 1 };
+  const className = "switch-wrapper";
+
+  return (
+    <AnimatedRoute
+      atEnter={atEnterOrLeave}
+      atLeave={atEnterOrLeave}
+      atActive={atActive}
+      className={className}
+      path={path}
+      exact={exact}
+      component={component}
+    />
+  );
+}
+
 function App() {
+  
+
   return (
     <Router>
       <div className="App">
-        <Route path="/" exact component={BookingsTimeline} />
-        <Route path="/rooms/" component={Rooms} />
-        <Route path="/settings/" component={Settings} />
+        <AnimatedRouteHOC path="/" exact component={BookingsTimeline} />
+        <AnimatedRouteHOC path="/rooms/" component={Rooms} />
+        <AnimatedRouteHOC path="/settings/" component={Settings} />
         <FooterMenu />
       </div>
     </Router>
